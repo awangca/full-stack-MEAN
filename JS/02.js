165967822d6d81b82534b0ea69027e9de08c5dab
gmofeng <gmofeng@gmail.com>
2017-10-11 17:11:28 -0700

    JQuery added

---
+++ JS/02.js
@@ -0,0 +1,16 @@
+// alert("AA");
+
+// var _a= "An .. error";
+
+// alert(_a);
+
+var clsBanner = document.getElementById('closeBanner');
+
+var topBanner = document.getElementById('topBanner');
+
+
+	clsBanner.onclick = function () {
+
+		alert("Warning: Want to close it? ");
+		topBanner.className= "hide";
+}
--- JS/1.js
+++ JS/1.js
@@ -1,1 +1,8 @@
-alert("Danger!!");
+// alert("Danger!!");
+console.log("This is the console");
+console.error("This is an error");
+console.warn("STOP");
+
+document.write("<h2> No issues </h2>");
+prompt("INPUT ");
+confirm("Right!");
